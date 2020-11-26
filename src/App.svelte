fdcb33687b6e591079213cd526304a6436c7f08d
Muhammad Naufal <46437079+naufalist@users.noreply.github.com>
2020-11-26 14:22:47 +0700

    modif App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,12 @@
 <script>
-	export let name;
+  import Home from './pages/Home.svelte';
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-<footer>&copy; 2020 All right reserved.</footer>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  :global(div) {
+    border: 0.5em solid #1a1a1a;
+    padding: 1em;
+  }
 </style>
+
+<Home />
