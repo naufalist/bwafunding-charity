15ec2554f61908b9f5fb0aebd872ba68041544bd
Muhammad Naufal <46437079+naufalist@users.noreply.github.com>
2020-11-26 15:29:01 +0700

    update/modify components

--- src/data/charities.js
+++ src/data/charities.js
@@ -5,9 +5,9 @@ module.exports = {
       title: 'First Charity Project',
       category: 'Money',
       thumbnail: 'https://live.staticflickr.com/7721/17102022497_84b7c9c342_w.jpg',
-      pledged: 0,
+      pledged: 99000,
       target: 100000,
-      date_end: +new Date('30 August 2020'),
+      date_end: +new Date('30 November 2020'),
       profile_photo: 'https://live.staticflickr.com/4027/4357722810_7136f4a9e9_s.jpg',
       profile_name: 'Viroide Bueno',
       no_pledges: 0
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,20 +1,18 @@
 <script>
   import CharityList from "../components/CharityList.svelte";
+  import Header from '../components/Header.svelte';
+  import Welcome from '../components/Welcome.svelte';
+  import Promo from '../components/Promo.svelte';
+  import Footer from '../components/Footer.svelte';
+
   import { charities } from "../data/charities.js";
 
   let title = "Charity";
 
-  // let charities = ["Charity 1", "Charity 2", "Charity 3", "Charity 4"];
 </script>
 
-<style>
-  h1 {
-    font-size: 3em;
-    color: #e2071c;
-  }
-</style>
-
-<div>
-  <h1>{title}</h1>
-  <CharityList {charities} />
-</div>
+<Header />
+<Welcome />
+<CharityList {charities} />
+<Promo />
+<Footer />
