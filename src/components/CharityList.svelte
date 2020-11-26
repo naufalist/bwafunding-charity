557ef489262e542e68858d96f143436d0ac580f9
Muhammad Naufal <46437079+naufalist@users.noreply.github.com>
2020-11-26 20:00:11 +0700

    Single Page Application

--- src/components/CharityList.svelte
+++ src/components/CharityList.svelte
@@ -141,10 +141,10 @@
 
                 <span class="xs-separetor"></span>
 
-                <button data-toggle="modal" data-target="#exampleModal"
-                  class="btn btn-primary btn-block" on:click={handleButton}>
+                <a href="/donation/{charity.id}" data-toggle="modal" data-target="#exampleModal"
+                  class="btn btn-primary btn-block">
                   Donate This Cause
-                </button>
+                </a>
               </div><!-- .xs-item-content END -->
             </div><!-- .xs-popular-item END -->
           </div>
--- src/data/charities.js
+++ src/data/charities.js
@@ -2,16 +2,17 @@ module.exports = {
   charities: [
     {
       id: 1,
-      title: 'First Charity Project',
-      category: 'Money',
-      thumbnail: 'https://live.staticflickr.com/7721/17102022497_84b7c9c342_w.jpg',
+      title: "First Charity Project",
+      category: "Money",
+      thumbnail:
+        "https://webmatika.com/wp-content/uploads/2020/05/17102022497_84b7c9c342_c.jpg",
       pledged: 99000,
       target: 100000,
-      date_end: +new Date('30 November 2020'),
-      profile_photo: 'https://live.staticflickr.com/4027/4357722810_7136f4a9e9_s.jpg',
-      profile_name: 'Viroide Bueno',
-      no_pledges: 0
-
-    }
-  ]
-}
+      date_end: +new Date("30 November 2020"),
+      profile_photo:
+        "https://live.staticflickr.com/4027/4357722810_7136f4a9e9_s.jpg",
+      profile_name: "Viroide Bueno",
+      no_pledges: 0,
+    },
+  ],
+};
