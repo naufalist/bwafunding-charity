cdb0d0ae9c8daa5fcffa91dbee8c171d2f344af3
Muhammad Naufal <46437079+naufalist@users.noreply.github.com>
2020-11-26 14:23:06 +0700

    add new component

---
+++ src/pages/Home.svelte
@@ -0,0 +1,20 @@
+<script>
+  import CharityList from "../components/CharityList.svelte";
+  import { charities } from "../data/charities.js";
+
+  let title = "Charity";
+
+  // let charities = ["Charity 1", "Charity 2", "Charity 3", "Charity 4"];
+</script>
+
+<style>
+  h1 {
+    font-size: 3em;
+    color: #e2071c;
+  }
+</style>
+
+<div>
+  <h1>{title}</h1>
+  <CharityList {charities} />
+</div>
