e7228a9cc79ae5aaafa0a30452665cdae5c70eae
Muhammad Naufal <46437079+naufalist@users.noreply.github.com>
2021-07-04 20:53:03 +0700

    API & Svelte Component Life Cycle

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,14 +1,35 @@
 <script>
+  // import {
+  //   onMount,
+  //   onDestroy,
+  //   beforeUpdate,
+  //   afterUpdate
+  // } from 'svelte';
+
   import CharityList from "../components/CharityList.svelte";
   import Header from '../components/Header.svelte';
   import Welcome from '../components/Welcome.svelte';
   import Promo from '../components/Promo.svelte';
   import Footer from '../components/Footer.svelte';
 
-  import { charities } from "../data/charities.js";
+  import {
+    charities
+  } from "../data/charities.js";
 
   let title = "Charity";
 
+  // onMount(function() {
+  //   console.log("onMount")
+  // });
+  // onDestroy(function() {
+  //   console.log("onDestroy")
+  // });
+  // beforeUpdate(function() {
+  //   console.log("beforeUpdate")
+  // });
+  // afterUpdate(function() {
+  //   console.log("afterUpdate")
+  // });
 </script>
 
 <Header />
